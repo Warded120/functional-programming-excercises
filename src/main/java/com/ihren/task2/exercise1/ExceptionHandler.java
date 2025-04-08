ebfb357756ff3fe49620c8ae800d2771ff46f9c8
Warded120 <ihren@softserveinc.com>
2025-04-08 14:59:47 +0300

    removed commented code

--- src/main/java/com/ihren/task2/exercise1/ExceptionHandler.java
+++ src/main/java/com/ihren/task2/exercise1/ExceptionHandler.java
@@ -11,29 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import java.util.function.Function;
 
-//@Slf4j
-//@Component
-//@RequiredArgsConstructor
-//public class ExceptionHandler {
-//
-//    private final DLQHandler dlqHandler;
-//    private final Metrics metrics;
-//
-//    public <I, O> Try<O> handle(Function<I, O> func, I input) {
-//        Function<ApplicationException, Try<O>> handleFunction = ex -> {
-//            log.error("An error occurred while processing the message", ex);
-//            dlqHandler.send(input, ex);
-//            metrics.incrementMessagesInDlqCounter();
-//            return Try.success(null);
-//        };
-//
-//        return Try.of(() -> func.apply(input))
-//                .recoverWith(MappingException.class, handleFunction::apply)
-//                .recoverWith(Exception.class, ex -> handleFunction.apply(new ApplicationException(ErrorCode.UNKNOWN, ex.getMessage(), ex)));
-//    }
-//
-//}
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
